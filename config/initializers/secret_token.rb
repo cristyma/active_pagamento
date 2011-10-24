47901691ad6b65748693f46d6954332c34a95629
cristiany maeda <cristyma@gmail.com>
2011-10-24 09:44:09 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivePagamento::Application.config.secret_token = 'e8600dd739d6512ac92241253857ceb5fa8d3a7f962aa71dfc4b6e1960b25986cbb30739c0c9d5d09968f7cbd52b12bf2a278697d437acf078ec9070f240c9f8'
