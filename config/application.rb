1feaf5da4819e78cc98b2cf0a0ea9ff0f17ae489
cristiany maeda <cristyma@gmail.com>
2011-10-24 09:59:57 -0200

    instalação do active_admin

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,8 @@ module ActivePagamento
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #heroku
+    config.assets.initialize_on_precompile = false
   end
 end
